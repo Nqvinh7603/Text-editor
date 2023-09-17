0a4eb5e3c6d99564c1ea2de86d90ba8d2f015c03
Nguyễn Quang Vinh <nqvinh7603@gmail.com>
2023-09-17 23:15:19 +0700

    init project

---
+++ src/main/java/com/texteditor/TextEditor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.texteditor;
+
+/**
+ *
+ * @author Admin
+ */
+public class TextEditor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
